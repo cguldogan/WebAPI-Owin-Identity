38709d88cc8582cdc6bf002c629c919b7b11aca4
can.guldogan <can.guldogan@CHAM8229>
2017-08-16 17:09:59 +0100

    Add project files.

---
+++ WebApi/App_Start/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using WebApi.Classes.Auth;
+
+[assembly: OwinStartup(typeof(WebApi.App_Start.Startup))]
+
+namespace WebApi.App_Start
+{
+    public class Startup
+    {
+        //Default Configuration Method
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    HttpConfiguration config = new HttpConfiguration();
+        //    WebApiConfig.Register(config);
+        //    app.UseWebApi(config);
+        //}
+
+
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+
+
+}
---
+++ WebApi/Classes/Auth/AuthContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Classes.Auth
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
